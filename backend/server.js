e443735e0a205176f71e21c698c69e9bc0f8a54e
raishan12 <muhammedraishan12@gmail.com>
2025-03-29 17:01:35 +0530

    contactbook initiated

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,15 @@ const queryString = require("querystring")
 
 const html1File = path.join(__dirname, "..", "frontend", "index.html")
 const html2File = path.join(__dirname, "..", "frontend", "index.html")
-const css1File = path.join(__dirname, "..", "frontend", "css", "style.css")
-// const css2File = path.join(__dirname, "..", "frontend", "css", "style2.css")
+const css1File = path.join(__dirname, "..", "frontend", "css", "style1.css")
+const css2File = path.join(__dirname, "..", "frontend", "css", "style2.css")
+const js1File = path.join(__dirname, "..", "frontend", "js", "index1.js")
+const js2File = path.join(__dirname, "..", "frontend", "js", "index2.js")
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url)
     if (req.method === "GET") {
-        if (parsedUrl.method === "/") {
+        if (parsedUrl.url === "/") {
             fs.readFile(html1File, (err, data) => {
                 if (err) {
                     res.writeHead(500)
@@ -23,8 +25,32 @@ const server = http.createServer((req, res) => {
                 }
             })
         }
-        if (parsedUrl.method === "/style1.css") {
+        if (parsedUrl.method === "/css/style1.css") {
             fs.readFile(css1File, (err, data) => {
+                if (err) {
+                    res.writeHead(500)
+                    res.end(err.message)
+                }else{
+                    res.writeHead(200,{"Content-Type":"text/css"})
+                    res.end(data)
+                }
+            })
+        }
+        if (parsedUrl.method === "/js/index1.js") {
+            fs.readFile(js1File, (err, data) => {
+                if (err) {
+                    res.writeHead(500)
+                    res.end(err.message)
+                }else{
+                    res.writeHead(200,{"Content-Type":"text/json"})
+                    res.end(data)
+                }
+            })
+        }
+
+
+        if (parsedUrl.url === "/addpage") {
+            fs.readFile(html2File, (err, data) => {
                 if (err) {
                     res.writeHead(500)
                     res.end(err.message)
@@ -34,6 +60,31 @@ const server = http.createServer((req, res) => {
                 }
             })
         }
+        if (parsedUrl.method === "/css/style2.css") {
+            fs.readFile(css2File, (err, data) => {
+                if (err) {
+                    res.writeHead(500)
+                    res.end(err.message)
+                }else{
+                    res.writeHead(200,{"Content-Type":"text/css"})
+                    res.end(data)
+                }
+            })
+        }
+        if (parsedUrl.method === "/js/index2.js") {
+            fs.readFile(js2File, (err, data) => {
+                if (err) {
+                    res.writeHead(500)
+                    res.end(err.message)
+                }else{
+                    res.writeHead(200,{"Content-Type":"text/json"})
+                    res.end(data)
+                }
+            })
+        }
+
+
+
     }
 })
 
